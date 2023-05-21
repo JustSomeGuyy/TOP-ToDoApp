49dc92d0af39e70619be560dda25e2b3404041de
Tyler Wojciechowski <106572294+JustSomeGuyy@users.noreply.github.com>
2023-05-21 01:28:19 -0500

    created most of the projects.js file that will build, display, and manage the projects and sub-tasks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import Projects, { projectArray, addProject } from "./modules/projects.js";
+import Projects, { projectArray, addProject, addProjectTasks } from "./modules/projects.js";
 
 let button = document.getElementById('addProject');
 
 button.addEventListener('click', addProject);
 
+let buttonTwo = document.getElementById('addTasks');
+
+buttonTwo.addEventListener('click', () => addProjectTasks(1));
+
 console.log(projectArray)
+
